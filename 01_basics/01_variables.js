fa7d848cb274aba0dd9c70d133166f7b495ef52e
Krishna Chaitanya <150346887+KRISHNA-TE@users.noreply.github.com>
2024-05-09 09:37:20 +0000

    Container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+ const acccountid = 12345
+
+ let accountPassword = "12"
+ var accountemail = "kris@google.com"
+/*
+Perfer not to use var
+because of issue in block Scope and functional scope
+*/
+ accountname = "Bengaluru"
+
+ console.table([acccountid,accountPassword,accountemail,accountname])
