bb58917abe99481bdd3812b317101cd11f0b2c80
Krishna Chaitanya <150346887+KRISHNA-TE@users.noreply.github.com>
2024-05-09 10:05:10 +0000

    container variables done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,10 +2,11 @@
 
  let accountPassword = "12"
  var accountemail = "kris@google.com"
+ let accountstate;
 /*
 Perfer not to use var
 because of issue in block Scope and functional scope
 */
  accountname = "Bengaluru"
 
- console.table([acccountid,accountPassword,accountemail,accountname])
+ console.table([acccountid,accountPassword,accountemail,accountname,accountstate])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict";// treats all js code newer version
+
+//alert(3 + 3) // we r using node js not browser
+
+//  code readability should be high
+
+//number ,bigint
+//string =>""
+//boolean
+//null=> standalone value
+//undefined =>
+//symbol =>unique
+
+//object
+
+//console.log(typeof undefined) //undefined
+//console.log(typeof null) //object
