ff1706787e62a31d7e12e656e983d8d47b5776cc
Krishna Chaitanya <150346887+KRISHNA-TE@users.noreply.github.com>
2024-05-10 15:49:51 +0530

    Add files via upload

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "33a";
+
+// console.log(typeof score);
+
+//"33" =>33
+//"33abc" => NaN
+//true => 1 ; false =>0
+
+let LoggedIn = "krishna"
+
+let booleanIsLoggedIn = Boolean(LoggedIn)
+// console.log(booleanIsLoggedIn);
+
+//1=>true ; 0=>false
+//"" =>false
+//"krishna" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+
+// ****************** Operation ****************
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+
+// console.log("1" + 2 + 2);
+
+console.log(+true);
---
+++ 01_basics/04_comprision.js
@@ -0,0 +1,5 @@
+console.log(null == 0);
+console.log(null >= 0);
+console.log(null != 0);
+
+// === /* if checks the datatypes and caomparison */
