d50d8aec77d3caabee9a0d4914daf89c39ea848c
Krishna Chaitanya <150346887+KRISHNA-TE@users.noreply.github.com>
2024-05-10 05:47:12 +0000

    data And Time

---
+++ 01_basics/07_datesinJs.js
@@ -0,0 +1,12 @@
+//Dates
+
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toLocaleString());
+
+myDate.toLocaleString('default',{
+    weekday:"long",
+
+})
