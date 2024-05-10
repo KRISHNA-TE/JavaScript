5c99b6ceb7f843eaec1579f1f43e24a8d54227a5
Krishna Chaitanya <150346887+KRISHNA-TE@users.noreply.github.com>
2024-05-10 05:16:56 +0000

    Nums and Math done

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,25 @@
+const score = 100;
+// console.log(score);
+
+const balance = new Number(200)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2)); //200.00
+
+ const hundred = 1000000
+// console.log(hundred.toLocaleString());//1,000,000
+// console.log(hundred.toLocaleString('en-IN'));//10,00,000
+
+
+// ++++++++++++++++++++++ Math +++++++++++++++++++++++++++
+
+console.log(Math)
+console.log(Math.abs(-4));
+
+console.log((Math.random()*10)+1)
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random()* (max-min+1))+min);
